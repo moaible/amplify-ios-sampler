b2fb657c8bc0f5b69524e6f1528380968eb824dc
moaible <moai.motodera@gmail.com>
2020-08-05 00:36:38 +0900

    Add initialized xcode project files

---
+++ Amplify-iOS-SamplerTests/Amplify_iOS_SamplerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Amplify_iOS_SamplerTests.swift
+//  Amplify-iOS-SamplerTests
+//
+//  Created by Motodera, Hiromi on 05/08/2020.
+//  Copyright © 2020 Motodera, Hiromi. All rights reserved.
+//
+
+import XCTest
+@testable import Amplify_iOS_Sampler
+
+class Amplify_iOS_SamplerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
