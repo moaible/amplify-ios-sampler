69704c6c625712558436ab93cb69db238e1f1515
moaible <moai.motodera@gmail.com>
2020-08-09 01:38:11 +0900

    Implement GraphQL mutation view

--- Amplify-iOS-Sampler/Samples/GraphQL API/GraphQLMutationViewController.swift
+++ Amplify-iOS-Sampler/Samples/GraphQL API/GraphQLMutationViewController.swift
@@ -8,7 +8,33 @@
 
 import Foundation
 import UIKit
+import Amplify
 
 class GraphQLMutationViewController: UIViewController, LibraryFeatureViewConvertible {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.createTodo()
+    }
+
+    func createTodo() {
+        let todo = Todo(name: "\(Date())", description: "todo description")
+        _ = Amplify.API.mutate(request: .create(todo)) { event in
+            switch event {
+            case .success(let result):
+                switch result {
+                case .success(let todo):
+                    print("Successfully created the todo: \(todo)")
+                case .failure(let graphQLError):
+                    print("Failed to create graphql \(graphQLError)")
+                }
+            case .failure(let apiError):
+                print("Failed to create a todo", apiError)
+            }
+        }
+    }
 }
