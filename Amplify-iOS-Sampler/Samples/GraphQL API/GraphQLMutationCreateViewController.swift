239f5a599fd1b9f11b2c2d74b7b6a77cfe940b7e
moaible <moai.motodera@gmail.com>
2020-08-10 11:53:50 +0900

    wip

---
+++ Amplify-iOS-Sampler/Samples/GraphQL API/GraphQLMutationCreateViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GraphQLMutationCreateViewController.swift
+//  Amplify-iOS-Sampler
+//
+//  Created by Motodera, Hiromi on 08/08/2020.
+//  Copyright © 2020 Motodera, Hiromi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Amplify
+import Hydra
+
+class GraphQLMutationCreateViewController: UIViewController, LibraryFeatureViewConvertible {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
